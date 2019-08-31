29884900e96324350b6ca36f21eb65fbf6a6ccca
Sam Kitchen <samkitchen94@gmail.com>
2019-08-31 16:06:52 +0100

    second test passing, new canvas editor now initializes with m amount of n length arrays

--- lib/canvas_editor.rb
+++ lib/canvas_editor.rb
@@ -1,7 +1,9 @@
 class CanvasEditor
+  attr_reader :unstyled_canvas
 
   def initialize(m, n)
+    @unstyled_canvas = Array.new(m){ Array.new(n, 'O') }
     @row_length = m
-    @column_length = n 
+    @column_length = n
   end
 end
--- spec/canvas_editor_spec.rb
+++ spec/canvas_editor_spec.rb
@@ -1,9 +1,13 @@
 require 'canvas_editor'
 
 describe CanvasEditor do
+  let(:canvas) { CanvasEditor.new(2, 4) }
   describe '#new' do
     it "initializes with length of rows and columns" do
       expect(CanvasEditor).to respond_to(:new).with(2).arguments
+    end
+    it "initializes with n amount of m length arrays" do
+      expect(canvas.unstyled_canvas).to eq([["O", "O", "O", "O"], ["O", "O", "O", "O"]])
     end
   end
 end
