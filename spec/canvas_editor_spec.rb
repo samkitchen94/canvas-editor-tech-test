901d0208551cfc09ed8625b7f485cee7c9eec60a
Sam Kitchen <samkitchen94@gmail.com>
2019-09-03 10:39:24 +0100

    refactored tests slightly

--- spec/canvas_editor_spec.rb
+++ spec/canvas_editor_spec.rb
@@ -2,11 +2,25 @@ require 'canvas_editor'
 
 describe CanvasEditor do
   canvas_editor = CanvasEditor.new
-  it "draws a canvas from users selection" do
+  before :each do
     canvas_editor.choose_selection("I 2 4")
-    expect { canvas_editor.choose_selection("S") }.to output("OOOO\nOOOO\n").to_stdout
   end
-  it "shows the help menu when user chooses ?" do
-    expect { canvas_editor.choose_selection("?") }.to output("This is the help menu\n").to_stdout
+  context "drawing canvas" do
+    it "draws a canvas from users selection" do
+      expect { canvas_editor.choose_selection("S") }.to output("OOOO\nOOOO\n").to_stdout
+    end
+  end
+
+  context "help menu" do
+    it "shows the help menu when user chooses ?" do
+      expect { canvas_editor.choose_selection("?") }.to output("This is the help menu\n").to_stdout
+    end
+  end
+
+  context "editing the canvas" do
+    it "changes a specific pixel in the canvas with the letter chosen" do
+      canvas_editor.choose_selection("L 2 2 C")
+      expect { canvas_editor.choose_selection("S") }.to output("OOOO\nOOOO\n").to_stdout
+    end
   end
 end
